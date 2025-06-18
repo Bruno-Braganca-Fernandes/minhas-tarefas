d46f4cbdf3e76865252e033da35f5b645ee8e315
bruno <bruno.b.fernandes12@hotmail.com>
2025-06-18 18:02:02 -0300

    feat: executando a função no componente para programar a remoção de uma tarefa.

--- src/store/reducers/tarefas.ts
+++ src/store/reducers/tarefas.ts
@@ -1,39 +1,51 @@
+// Configuração do Reducer para as tarefas
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import Tarefa from '../../models/Tarefa'
 import * as enums from '../../utils/enums/Tarefa'
 
-const tarefaSlice = createSlice({
+//Config de tipagem para que TarefasState tenha seus itens seguindo a configuração da classe model Tarefa.
+type TarefasState = {
+  itens: Tarefa[]
+}
+
+//Config para que o initialState tenha como valor os itens preenchidos na tipagem de TarefasState
+const initialState: TarefasState = {
+  itens: [
+    {
+      id: 1,
+      descricao: 'Estudar JavaScript revendo o exercício do módulo 7.',
+      prioridade: enums.Prioridade.NORMAL,
+      status: enums.Status.CONCLUIDA,
+      titulo: 'Estudar JavaScript'
+    },
+    {
+      id: 2,
+      descricao: 'Estudar material de apoio.',
+      prioridade: enums.Prioridade.NORMAL,
+      status: enums.Status.PENDENTE,
+      titulo: 'Estudar TypeScript'
+    },
+    {
+      id: 3,
+      descricao: 'Praticar a construção de uma landing page',
+      prioridade: enums.Prioridade.IMPORTANTE,
+      status: enums.Status.PENDENTE,
+      titulo: 'Estudar Bootstrap'
+    }
+  ]
+}
+
+const tarefasSlice = createSlice({
   name: 'tarefas',
-  initialState: [
-    new Tarefa(
-      'Estudar JavaScript',
-      enums.Prioridade.IMPORTANTE,
-      enums.Status.PENDENTE,
-      '',
-      1
-    ),
-    new Tarefa(
-      'Estudar TypeScript',
-      enums.Prioridade.URGENTE,
-      enums.Status.CONCLUIDA,
-      'Rever aula 2 do módulo',
-      2
-    ),
-    new Tarefa(
-      'Estudar React',
-      enums.Prioridade.URGENTE,
-      enums.Status.PENDENTE,
-      'Praticar o useEffect',
-      3
-    )
-  ],
+  initialState: initialState,
+  //Função responsável por remover cards de tarefas.
   reducers: {
     remover: (state, action: PayloadAction<number>) => {
-      state = state.filter((tarefa) => tarefa.id !== action.payload)
+      state.itens = state.itens.filter((tarefa) => tarefa.id !== action.payload)
     }
   }
 })
 
-export const { remover } = tarefaSlice.actions
+export const { remover } = tarefasSlice.actions // Exportando apenas a função remover de tarefasSlice
 
-export default tarefaSlice.reducer
+export default tarefasSlice.reducer
